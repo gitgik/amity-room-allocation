74b9fd471674ff3ad0fde34ec4203b030bf0cee9
andela-ggikera <githinji.gikera@andela.com>
2015-12-13 23:26:50 +0300

    Edit models

--- employees/model.py
+++ employees/model.py
@@ -1,12 +1,14 @@
 # models.py
 class Person(object):
-		def __init__(self, name):
-			  self.name = name
+    def __init__(self, name):
+        self.name = name
+
 
 class Staff(Person):
-		pass
-class Fellow(Person):
-		def __init__(self, name, is_living):
-				self.is_living = is_living
-				super(Fellow, self).__init__(name)
+    pass
 
+
+class Fellow(Person):
+    def __init__(self, name, is_living):
+        self.is_living = is_living
+        super(Fellow, self).__init__(name)
--- rooms/models.py
+++ rooms/models.py
@@ -1,28 +1,29 @@
 # models.py
 class Room(object):
-		def __init__(self, list_of_rooms):
-				self.list = list_of_rooms
-				self.members = []
+    def __init__(self, list_of_rooms):
+        self.list = list_of_rooms
+        self.members = []
 
-    #The size of the room
-		def size(self):
-				return len(self.members)
+    # The size of the room
+    def size(self):
+        return len(self.members)
 
-		def members_names(self):
-				member_list = [members.name for member in self.members]
-				return member_list
+    def members_names(self):
+        member_list = [member.name for member in self.members]
+        return member_list
 
-		# pre-populate the rooms with names
-		def pre_populate(self):
-				room_names = []
-				for i in self.list:
-						room_names.append(i)
-				return room_names
+    # pre-populate the rooms with names
+    def populate_room_names(self):
+        room_names = []
+        for i in self.list:
+            room_names.append(i)
+        return room_names
 
 
 class Office(Room):
-		maximum_size = 6
+    maximum_size = 6
+
 
 class LivingSpace(Room):
-		maximum_size = 4
+    maximum_size = 4
 
