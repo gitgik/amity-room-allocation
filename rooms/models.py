f4b3ccb65524544704d5eec4e3550c52a2b905ab
andela-ggikera <githinji.gikera@andela.com>
2015-12-13 21:42:04 +0300

    create room model for amity

---
+++ rooms/models.py
@@ -0,0 +1,28 @@
+# models.py
+class Room(object):
+		def __init__(self, list_of_rooms):
+				self.list = list_of_rooms
+				self.members = []
+
+    #The size of the room
+		def size(self):
+				return len(self.members)
+
+		def members_names(self):
+				member_list = [members.name for member in self.members]
+				return member_list
+
+		# pre-populate the rooms with names
+		def pre_populate(self):
+				room_names = []
+				for i in self.list:
+						room_names.append(i)
+				return room_names
+
+
+class Office(Room):
+		maximum_size = 6
+
+class LivingSpace(Room):
+		maximum_size = 4
+
