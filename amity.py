09cb16b9a865c6dadf2deb381c464f9da8a170da
andela-ggikera <githinji.gikera@andela.com>
2015-12-14 10:39:49 +0300

    Use dictionary of rooms to house room names

--- amity.py
+++ amity.py
@@ -5,23 +5,32 @@ import random
 import sys
 
 """ office names: will contain names of people after allocations """
-allegro, boma, valhalla, hogwarts = [], [], [], []
-krypton, oculus, gondolla = [], [], []
-amitoid, punta, borabora = [], [], []
-
-office_rooms = [
-    allegro, boma, valhalla, hogwarts, krypton, oculus,
-    gondolla, amitoid, punta, borabora]
+office_rooms = {
+    "allegro": [], "boma": [], "valhalla": [],
+    "hogwarts": [], "krypton": [], "oculus": [],
+    "gondolla": [], "amitoid": [], "punta": [], "borabora": []
+}
 
 """ living space names """
-Green, Blue, Yellow, Lilac = [], [], [], []
-Orange, White, Brown, Turquoise, Grey, Purple = [], [], [], [], [], []
-
-living_rooms = [
-    Green, Blue, Yellow, Lilac,
-    Orange, White, Brown, Turquoise, Grey, Purple
+living_rooms = {
+    'Green': [], 'Blue': [], 'Yellow': [], 'Lilac': [],
+    'Orange': [], 'White': [], 'Brown': [],
+    'Turquoise': [], 'Grey': [], 'Purple': []
+}
+""" create a list of rooms (both office and living space)
+    which will be used to obtain the key to their respective
+    dictionary definitions during allocations
+"""
+office_list = [
+    'allegro', 'boma', 'valhalla',
+    'hogwarts', 'krypton', 'oculus', 'gondolla',
+    'amitoid', 'punta', 'borabora'
 ]
 
+livingspace_list = [
+    'Green', 'Blue', 'Yellow', 'Lilac',
+    'Orange', 'White', 'Brown', 'Turquoise', 'Grey', 'Purple'
+ ]
 
 class Amity(object):
     def print_rooms(self):
@@ -43,35 +52,41 @@ class Amity(object):
         employees = [
             employees.rstrip('\n') for employees in open(sys.argv[1], 'r')
         ]
-
+        index = 0
         """ loop through each person to determine their affiliations """
         for i in employees:
-            persons_description = [x.rstrip() for x in i.split(" ")]
-            """ allocate to a random office """
-            for random_room in room_index:
+                chosen_room = room_index[index]
+                persons_description = [x.rstrip() for x in i.split(" ")]
+
                 """ check whether they are fellows or employees """
                 if persons_description[2] == "Staff":
                     """ allocate only office space """
-                    office_names[random_room].append(
+                    key = office_list[chosen_room]
+                    office_names[key].append(
                         persons_description[0] + ' ' + persons_description[1])
-                    print office_names[random_room]
-                else:
-                    if persons_description[3] == "Y":
+                elif persons_description[2] == "Fellow":
+                    okey = office_list[chosen_room]
+                    lkey = livingspace_list[chosen_room]
+                    if len(persons_description) >= 4:
                         """ fellow needs a place to live AND office """
-                        office_names[random_room].append(
+                        office_names[okey].append(
                             persons_description[0] +
                             ' ' + persons_description[1])
-                        living_names[random_room].append(
+                        living_names[lkey].append(
                             persons_description[0] +
                             ' ' + persons_description[1])
                     else:
                         """ fellow does not need accomodation """
-                        office_names[random_room].append(
+                        office_names[okey].append(
                             persons_description[0] +
                             ' ' + persons_description[1])
+
+
+                index += 1
                         # maximum = office.maximum_size
                         # rand = round(random.random() * maximum)
 
+        print office_names
 amity = Amity()
 amity.print_rooms()
 amity.allocate_room()
