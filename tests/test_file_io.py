663e00e4dcef66b3aedf4b8ab49aeb9f5ac5d2aa
andela-ggikera <githinji.gikera@andela.com>
2015-12-21 22:20:34 +0300

    add file io test

---
+++ tests/test_file_io.py
@@ -0,0 +1,22 @@
+# file io test
+import unittest
+import nose
+import random
+from employees.model import Person
+from amity import Amity
+
+# file path to the input .txt file containing people
+file_path = 'input.txt'
+persons = []
+
+
+class FileInputTestCase(unittest.TestCase):
+    """ tests file IO to the program """
+
+    def test_parsing_file(self):
+        persons = Amity.get_people_from_file(file_path)
+        self.assertEquals(len(persons), 43)
+        self.assertIsInstance(persons[random.randint(0, 30)], Person)
+
+if __name__ == '__main__':
+    nose.run()
